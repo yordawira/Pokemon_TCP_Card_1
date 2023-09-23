f783f6910cac39d1031b1aea7aa97f135478c949
yordawira <wiraaswin275@gmail.com>
2023-09-23 20:12:51 +0700

    Add fixing gradel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
-    id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("com.google.dagger.hilt.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
- def API_BASE ("https://pokeapi.co/api/v2/")
+
+val API_BASE = "https://pokeapi.co/api/v2/"
+
+
 
 android {
     namespace = "com.catty.pokemon_tcp_card_1"
@@ -20,6 +21,8 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        manifestPlaceholders
+        var can_export_components = false
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -28,14 +31,19 @@ android {
     }
 
     buildTypes {
+        debug {
+            versionNameSuffix = "-debug"
+            buildConfigField("String", "API_BASE", "\"$API_BASE\"")
+
+            manifestPlaceholders["can_export_components"] = "true"
+        }
         release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            buildConfigField("String", "API_BASE", "\"$API_BASE\"")
+            var minifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -49,18 +57,13 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.7.0")
+    implementation ("com.google.android.material:material:1.9.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Retrofit
@@ -72,14 +75,16 @@ dependencies {
 
     // Coroutine Lifecycle Scopes
     // https://developer.android.com/topic/libraries/architecture/coroutines#lifecyclescope
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Compose
     // https://developer.android.com/jetpack/compose
-    implementation ("androidx.compose.material:material:1.3.1")
-    implementation ("androidx.navigation:navigation-compose:2.5.3") // Needed for hilt navigation
+
+    implementation ("androidx.compose.material:material:1.5.1")
+    implementation ("androidx.navigation:navigation-compose:2.7.3") // Needed for hilt navigation
     implementation ("com.google.accompanist:accompanist-swiperefresh:0.26.5-rc")
+
     // TODO: Try to use latest version of accompanist swiperefresh, not too much information :(
 
     //Dagger - Hilt
@@ -96,10 +101,10 @@ dependencies {
     implementation ("androidx.test:core:1.5.0")
     testImplementation ("junit:junit:4.13.2")
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+    testImplementation ("androidx.arch.core:core-testing:2.2.0")
     testImplementation ("org.mockito:mockito-core:4.4.0")
     testImplementation ("org.mockito:mockito-inline:3.11.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androwidx.test.espresso:espresso-core:3.5.1")
 
 }
